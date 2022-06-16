7b293fb0e857e1eb82eed5c2dc9b99a747d494eb
Gabriel <gxrb98@gmail.com>
2022-06-16 11:52:16 -0400

    Modificaciones

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
 import './Styles/sendMessage.css'
 import emailjs from 'emailjs-com'
 
@@ -7,31 +7,31 @@ const SendMessage = () => {
     const sendEmail = (e) => {
         e.preventDefault()
         emailjs.sendForm('contact_service', 'contact_form', e.target, 'FeauU2-plERrxk0An')
-                    .then(function() {
-                        console.log('SUCCESS!');
-                    }, function(error) {
-                        console.log('FAILED...', error);
-                    });
-                    e.target.reset()
+            .then(function () {
+                console.log('SUCCESS!');
+            }, function (error) {
+                console.log('FAILED...', error);
+            });
+        e.target.reset()
     }
     return (
         <Fragment>
             <div className='wrapper'>
-            <header>
-                <h1>Thanks for taking the time to reach out</h1>
-            </header>
-            <form className='form' onSubmit={sendEmail}>
-                <label for='name'>Name</label>
-                <input className='textbox' name='name' type='text'/>
-                <label for='email'>Email</label>
-                <input className='textbox' name='email' type='email'/>
-                <label for='subject'>Subject</label>
-                <input className='textbox' name='subject' type='text'/>
-                <label for='message'>Message</label>
-                <textarea className='textarea' name='message'>
-                </textarea>
-                <input type='submit' id='btn-submit'  value='Submit'/>
-            </form>
+                <header>
+                    <h1>Thanks for taking the time to reach out</h1>
+                </header>
+                <form className='form' onSubmit={sendEmail}>
+                    <label htmlfor='name'>Name</label>
+                    <input className='textbox' name='name' type='text' />
+                    <label htmlfor='email'>Email</label>
+                    <input className='textbox' name='email' type='email' />
+                    <label htmlfor='subject'>Subject</label>
+                    <input className='textbox' name='subject' type='text' />
+                    <label htmlfor='message'>Message</label>
+                    <textarea className='textarea' name='message'>
+                    </textarea>
+                    <input type='submit' id='btn-submit' value='Submit' />
+                </form>
             </div>
         </Fragment>
     )
